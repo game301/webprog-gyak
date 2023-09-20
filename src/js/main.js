cf86a51b4f0881182e6f53be867254d996ac4dd6
game301 <hurkaparty@gmail.com>
2023-09-20 16:34:59 +0200

    added darkmode icons, css variables

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 //"use strict"
 import { timeline } from "./animation.js"
 //console.log(timeline)
+
+document.addEventListener("click", () => {
+    const darkModeButton = document.querySelector("#nav-dark-mode-toggle")
+    console.log(darkModeButton)
+    darkModeButton.firstElementChild.classList.toggle("fa-sun")
+    darkModeButton.firstElementChild.classList.toggle("fa-moon")
+})
