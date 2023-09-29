e64ac6eed17274610bd80a1d3b227463173bcfc7
game301 <hurkaparty@gmail.com>
2023-09-29 15:15:37 +0200

    added cult figurine, nav li underline animation

--- src/js/animation.js
+++ src/js/animation.js
@@ -9,3 +9,44 @@ timeline
         y: "-10%",
         ease: "ease-in",
     })
+    .from(
+        "#cult-title",
+        {
+            opacity: 0,
+            scale: 2,
+            //boxShadow: "0px 0px 10px 10px rgba(255,255,255,1)",
+            //textShadow: "10px 25px 50px rgba(255,255,255,1)",
+            filter: "drop-shadow(-30px 20px 20px)",
+
+            //backgroundColor: "rgba(255,255,255,1)",
+            fontWeight: "600",
+            ease: "ease-in",
+            duration: 1.5,
+        },
+        "<"
+    )
+
+    .to(
+        "#cult-sylphie",
+        {
+            opacity: 1,
+            duration: 0,
+        },
+        "<1"
+    )
+    .to(
+        "#cult-roxy",
+        {
+            opacity: 1,
+            duration: 0,
+        },
+        "<0.5"
+    )
+    .to(
+        "#cult-eris",
+        {
+            opacity: 1,
+            duration: 0,
+        },
+        "<0.5"
+    )
